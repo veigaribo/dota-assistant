4173b9d3d593a70f472d09ebbe6c2e49c6c465da
Gabriel Lopes Veiga <gabriel.veiga@compasso.com.br>
2020-12-11 18:22:27 -0300

    remove unnecessary scripts

--- scripts/makeconfig.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-DIR="$(dirname "$0")"
-
-$DIR/makecredentials.py -y && $DIR/makeendpoints.py -y
--- scripts/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-DIR="$(dirname "$0")"
-
-$DIR/makeconfig.sh && python -m rasa run
